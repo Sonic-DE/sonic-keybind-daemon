Sonic-DE/sonic-globalacceld

Serialize empty QKeySequence object as none
When doing e.g. KGlobalAccel::setGlobalShortcut(QKeySequence()) we
get QList(QKeySequence()) for defaultShortcuts.

This serializes as an empty string. Functionally it's the same as
no sequence though, so serialize it as 'none'.

Otherwise we are in a situation where we sometimes write "" and sometimes
"none" to the file, which is weird

src/component.cpp
@@ -40,6 +40,11 @@ QString Component::stringFromKeys(const QList<QKeySequence> &keys)
     if (keys.isEmpty()) {
         return QStringLiteral("none");
     }
+
+    if (keys == QList{QKeySequence()}) {
+        return QStringLiteral("none");
+    }
+
     QString ret;
     for (const QKeySequence &key : keys) {
         ret.append(key.toString(QKeySequence::PortableText));

Sonic-DE/sonic-keybind-daemon

src/component.cpp
@@ -42,6 +42,10 @@ QString Component::stringFromKeys(const QSet<QKeySequence> &keys)
     QList<QKeySequence> sortedKeys(keys.begin(), keys.end());
     std::sort(sortedKeys.begin(), sortedKeys.end());
 
+    if (keys == QSet{QKeySequence()}) {
+        return QStringLiteral("none");
+    }
+
     QString ret;
     for (const QKeySequence &key : sortedKeys) {
         ret.append(key.toString(QKeySequence::PortableText));